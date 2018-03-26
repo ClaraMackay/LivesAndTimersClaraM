527d6d73467b3ba00d6bc2aacf24fab5142a3caa
Clara Mackay <clara.mackay@ocsbstudent.ca>
2018-03-26 11:07:04 -0400

    changes

--- main.lua
+++ main.lua
@@ -30,9 +30,59 @@ local incorrectObject
 local pointsObject
 local points = 0
 local formula
+
+-- variables for the timer
+local totalSeconds = 5
+local secondsLeft = 5
+local clockText
+local countDownTimer
+
+local lives = 3 
+local heart1 
+local heart2
+local heart3
+local heart4
+
 -----------------------------------------------------------------
 --LOCAL FUNCTIONS
 -----------------------------------------------------------------
+
+local function UpdateTime()
+	-- decrement the number of seconds
+	secondsLeft = secondsLeft - 1
+
+	-- display the number of seconds left in the clock object
+	clockText.text = secondsLeft .. ""
+
+	if (secondsLeft == 0 ) then 
+		-- reset the number of seconds left 
+		secondsLeft = totalSeconds 
+		lives = lives - 1 
+
+		-- ***IF THERE ARE NO NUMBERS LEFT, PLAY A LOSE SOUND, SHOW A 
+		-- "YOU LOSE" IMAGE AND CANCEL THE TIMER, REMOVE THE THIRD 
+		-- HEART BY  MAKING IT INVISIBLE
+		if (lives == 4) then 
+			heart4.isVisible = false 
+		elseif (lives == 3) then 
+			heart3.isVisible = false
+		elseif (lives == 2) then 
+			heart2.isVisible = false
+		elseif (lives == 1) then 
+			heart1.isVisible = false
+		end
+
+		--*** CALL THE FUNCTION TO ASK A NEW QUESTION
+
+	end
+end
+
+-- function that calls the timer 
+local function StartTimer()
+	-- create a countdown timer that loops infinitely 
+	countDownTimer = timer.performWithDelay(1000, UpdateTime, 0)
+end
+
 local function AskQuestion()
 
 	-- generate 2 random nuymbers between a max. and a min. number
@@ -43,7 +93,7 @@ local function AskQuestion()
 
 	formula = math.random(0, 2)
 
-if (fomula == 0) then
+if (formula == 0) then
 
 	correctAnswer = randomNumber1 + randomNumber2
 
@@ -116,7 +166,7 @@ function NumericFieldListener( event )
 			else 
 				incorrectObject.isVisible = true 
 
-				timer.performWithDelay(4000, HideCorrect)
+				timer.performWithDelay(4000, HideInorrect)
 
 		end
 	end
@@ -127,6 +177,22 @@ end
 --------------------------------------------------------------------------------
 --OBJECT CREATION
 --------------------------------------------------------------------------------
+-- create the lives to display on the screen
+heart1 = display.newImageRect("Images/heart.png", 90, 90)
+heart1.x = display.contentWidth * 7 / 8
+heart1.y = display.contentHeight * 1 / 7
+
+heart2 = display.newImageRect("Images/heart.png", 90, 90)
+heart2.x = display.contentWidth * 6 / 8
+heart2.y = display.contentHeight * 1 / 7
+
+heart3 = display.newImageRect("Images/heart.png", 90, 90)
+heart3.x = display.contentWidth * 5 / 8
+heart3.y = display.contentHeight * 1 / 7
+
+heart4 = display.newImageRect("Images/heart.png", 90, 90)
+heart4.x = display.contentWidth * 4 / 8
+heart4.y = display.contentHeight * 1 / 7
 
 -- displays a question ands sets the colour 
 questionObject = display.newText("", display.contentWidth/4, display.contentHeight/2, "Georgia", 80)
